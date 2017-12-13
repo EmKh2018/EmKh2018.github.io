6840f938e70e06cdb23a71e8e71bfb9267cd9f67
Khouli <kh@win.local>
2017-12-13 18:23:01 +0100

    translate to french

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Apr 13, 2018 16:00:25").getTime();
+		var countDownDate = new Date("Apr 14, 2018 16:00:25").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
@@ -274,10 +274,10 @@
 		// + minutes + "Minutes " + seconds + "Seconds ";
 
 		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>days</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
+		document.getElementById("jours").innerHTML = days +" <small>days</small>";
+		document.getElementById("heures").innerHTML = hours + " <small>hours</small> ";
 		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+		document.getElementById("secondes").innerHTML = seconds + " <small>seconds</small> ";
 
 		// If the count down is finished, write some text 
 		if (distance < 0) {
